835a75d3b0eee5d81e7945b0b804f41bc7f262d7
hdkiranbharadwaj <104179274+hdkiranbharadwaj@users.noreply.github.com>
2023-01-04 22:01:31 +0530

    Create Find First and Last Position of Element in Sorted Array.c

---
+++ Find First and Last Position of Element in Sorted Array.c
@@ -0,0 +1,56 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int Lower(int mid,int* nums)
+{
+    if(mid==0||*(nums+mid)!=*(nums+mid-1))
+    {
+        return mid;
+    }
+    else
+    return Lower (mid-1,nums);
+}
+int Upper(int mid,int* nums,int n)
+{
+    if(mid==n-1||(*(nums+mid)!=*(nums+mid+1)))
+    {
+        return mid;
+    }
+    else
+    return Upper(mid+1,nums,n);
+}
+int* searchRange(int* nums, int numsSize, int target, int* returnSize){
+int* array=(int*)malloc(2*sizeof(int));
+int flag=0;
+int end=numsSize-1;
+int start=0;
+int mid;
+    if(numsSize==0)
+        goto L;
+while(start<=end)
+{
+    mid=(end+start)/2;
+    if(*(nums+mid)==target) {printf("%d",mid); flag=1;break;}
+    else if(target>*(nums+mid)) start=mid+1;
+    else end=mid-1;
+}
+if(numsSize==1&&*(nums)==target)
+{ 
+    *(array)=*(array+1)=0; 
+     *returnSize=2;
+     return array; 
+}
+
+    if(flag==0) {
+       
+   L:  *(array)=*(array+1)=-1; 
+     *returnSize=2;
+     return array; 
+     }
+    
+    *array= Lower(mid,nums);
+    *(array+1)= Upper(mid,nums,numsSize);
+    *returnSize=2;
+    return array;
+
+}
